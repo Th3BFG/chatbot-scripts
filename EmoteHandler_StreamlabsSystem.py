62257071269c633c4464afd069e1448c88f8ddef
th3bfg <th3bfg@gmail.com>
2018-05-20 18:41:20 -0400

    Added basic cooldown to emote response

--- EmoteHandler_StreamlabsSystem.py
+++ EmoteHandler_StreamlabsSystem.py
@@ -11,27 +11,50 @@ Description = "Emote Handler for Streamlabs Bot"
 Creator = "th3bfg"
 Version = "0.0.1"
 
+# Handler Variables
+path = os.path.dirname(__file__)
 configFile = "config.json"
 settings = {}
+timeFromLastTick = time.time()
+cooldownActive = False;
+threadsKeepAlive = True;
+cooldown = {
+    "fileName": "cooldown.txt",
+    "timeLeft": 0,
+    "running": False,
+}
 
+# Script Methods
 def ScriptToggled(state):
+	global threadsKeepAlive
+	if state:
+		threadsKeepAlive = True
+	else:
+		ResetCooldown()
+		threadsKeepAlive = False
 	return
 
 def Init():
-	global settings
+	global settings, path, configFile
 
 	path = os.path.dirname(__file__)
 	try:
 		with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
 			settings = json.load(file, encoding='utf-8-sig')
 	except:
-		settings = {}
+		settings = {
+			"cdInterval": 3,
+		}
 
 def Execute(data):
+	global settings, cooldown
 	outputMessage = ""
 	if data.IsChatMessage():
 		if data.GetParam(0) == "Kreygasm":
-			outputMessage = "Kreygasm"
+			# Check if user has a cooldown
+			if not cooldownActive:
+				outputMessage = "Kreygasm"
+				StartCooldown()
 	Parent.SendStreamMessage(outputMessage)
 	return
 
@@ -46,3 +69,29 @@ def OpenReadMe():
 
 def Tick():
 	return
+	
+# Helpers
+def StartCooldown():
+	global cooldown, settings, cooldownActive
+	cooldown["running"] = True
+	if not cooldownActive:
+		threading.Thread(tartget=CooldownThread, args()).start()
+		
+def ResetCooldown():
+	global cooldown, settings
+	cooldown["timeLeft"] = settings["cdInterval"] * 60
+	cooldown["running"] = False;
+	with codecs.open(os.path.join(path, cooldown["fileName"]), encoding='utf-8-sig', mode='w+') as file:
+		file.write(" ")
+		
+def CooldownThread():
+	global cdVariables, cooldown, settings, timeFromLastTick, cooldownActive, threadsKeepAlive
+	cooldownActive = True
+	while cooldown["running"] and threadsKeepAlive
+		cooldown["timeLeft"] -= 1
+		if cooldown["currentTime"] < 0:
+			cooldown["timeLeft"] = False
+		time.sleep(1)
+	cooldownActive = False
+		
+	
